bc0b9cfb0f34163062355a990ff1eefd5869e1c5
CountrySideEngineer <deep-sleeper@nifty.com>
2020-03-20 06:36:16 +0900

    Change window/view file name. -Change start up window file name and its name of class. -Add git ignore.

--- dev/dev/CStubMKGui/View/CStubMkGui.xaml.cs
+++ dev/dev/CStubMKGui/View/CStubMkGui.xaml.cs
@@ -18,9 +18,9 @@ namespace CStubMKGui
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CStubMkGui : Window
     {
-        public MainWindow()
+        public CStubMkGui()
         {
             InitializeComponent();
         }
