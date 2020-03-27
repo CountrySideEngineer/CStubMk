221435dd0fb2acf5aecbdfc5149743107b1ed84c
CountrySideEngineer <deep-sleeper@nifty.com>
2020-03-28 07:48:22 +0900

    Add classes to parse file. -Add classes below to parse function definition in microsoft excel file.  --IParser   ---Interface of parser.  --AParser   ---Abstract interface of parser.  --CExcelParser   ---Parser class to parse excel file.

---
+++ dev/dev/CStubMKGui/Model/AParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CStubMKGui.Model
+{
+    public abstract class AParser : IParser
+    {
+        #region Other methods and private properties in calling order
+        /// <summary>
+        /// Interface to extract information and parameters of stub to creat stub function.
+        /// </summary>
+        /// <param name="functionDefinition">Path of file the function information.</param>
+        /// <returns>Parameters for function.</returns>
+        public abstract IEnumerable<Param> Parse(String functionDefinition);
+        #endregion
+    }
+}
