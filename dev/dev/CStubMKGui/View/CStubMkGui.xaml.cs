732ed3494589492b15c77d9ebbe69fa8763f8ae0
CountrySideEngineer <deep-sleeper@nifty.com>
2020-03-28 16:30:58 +0900

    Add drag and drop function.

--- dev/dev/CStubMKGui/View/CStubMkGui.xaml.cs
+++ dev/dev/CStubMKGui/View/CStubMkGui.xaml.cs
@@ -24,5 +24,57 @@ namespace CStubMKGui
         {
             InitializeComponent();
         }
+
+        private void StubDefFilePathTextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(System.Windows.DataFormats.FileDrop, true))
+            {
+                e.Effects = System.Windows.DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = System.Windows.DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void StubDefFilePathTextBox_Drop(object sender, DragEventArgs e)
+        {
+            var dropFiles = e.Data.GetData(System.Windows.DataFormats.FileDrop) as string[];
+            if (null == dropFiles)
+            {
+                return;
+            }
+            else
+            {
+                StubDefFilePathTextBox.Text = dropFiles[0];
+            }
+        }
+
+        private void StubOutputPathTextBox_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void StubOutputPathTextBox_Drop(object sender, DragEventArgs e)
+        {
+            var dropFiles = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (null == dropFiles)
+            {
+                return;
+            }
+            else
+            {
+                StubOutputPathTextBox.Text = dropFiles[0];
+            }
+        }
     }
 }
