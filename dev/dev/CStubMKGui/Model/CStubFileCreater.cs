e7d035813630039ded9b6f674b44ab5dfb60e1d8
CountrySideEngineer <deep-sleeper@nifty.com>
2020-05-03 07:11:57 +0900

    Add classes to create source and header file from parameters. -Add ASourceFileCreater class, abstract class to create file from parameters, has abstract method, GetCodeWriter and GetCodeCreater, and has "concrete" methods to create code and write them into source files. -Add StubSourceFileCreater and StubHeaderFileCreater class, inherits ASourceFileCreater class. -Update CStubFileCreater class to create list of classes inherit ASourceFileCreater class and call their "Create" method to create stub source and header files.

--- dev/dev/CStubMKGui/Model/CStubFileCreater.cs
+++ dev/dev/CStubMKGui/Model/CStubFileCreater.cs
@@ -24,87 +24,27 @@ namespace CStubMKGui.Model
         /// </summary>
         /// <param name="outputPath">Path to output source file.</param>
         /// <param name="parameters">Parameters to create stub.</param>
-        public virtual void Create(String outputPath, IEnumerable<Param> parameters)
+        public virtual void Create(string outputPath, IEnumerable<Param> parameters)
         {
-            this.CreateSourceFile(outputPath, parameters);
-            this.CreateHeaderFile(outputPath, parameters);
+            this.CreateStub(outputPath, parameters);
         }
 
-        /// <summary>
-        /// Create source file of stub in C language style.
-        /// </summary>
-        /// <param name="outputPath">Path to directory to output source file.</param>
-        /// <param name="parameters">Parameters to create stub.</param>
-        protected void CreateSourceFile(string outputPath, IEnumerable<Param> parameters)
+        protected virtual void CreateStub(string outputPath, IEnumerable<Param> parameters)
         {
-            var codes = this.CreateSourceCode(parameters);
-            this.WriteSourceCodes(outputPath, codes);
+            var sourceFileCreaters = new List<ISourceFileCreater>
+            {
+                new StubSourceFileCreater(),
+                new StubHeaderFileCreater()
+            };
+            this.CreateStub(outputPath, sourceFileCreaters, parameters);
         }
 
-        /// <summary>
-        /// Create list of codes to write to source code file.
-        /// </summary>
-        /// <param name="parameters">Parameters to output soruce file.</param>
-        /// <returns>List fo codes to write to source.</returns>
-        protected IEnumerable<string> CreateSourceCode(IEnumerable<Param> parameters)
+        protected virtual void CreateStub(string outputPath, IEnumerable<ISourceFileCreater> createres, IEnumerable<Param> parameters)
         {
-            var creater = new CLangSourceStubCodeCreater();
-            var codes = this.CreateCodes(creater, parameters);
-
-            return codes;
-        }
-
-        protected void CreateHeaderFile(string outputPath, IEnumerable<Param> parameters)
-        {
-            var codes = this.CreateHeaderCode(parameters);
-            this.WriteHeaderCodes(outputPath, codes);
-        }
-
-        protected IEnumerable<string> CreateHeaderCode(IEnumerable<Param> parameters)
-        {
-            var creater = new CLangHeaderStubCodeCreater();
-            var codes = this.CreateCodes(creater, parameters);
-
-            return codes;
-        }
-
-        /// <summary>
-        /// Create list of codes to write to source code file.
-        /// </summary>
-        /// <param name="codeCreater">ICodeCreater concrete object to create code.</param>
-        /// <param name="parameters">Parameters to output soruce file.</param>
-        /// <returns>List fo codes to write to source.</returns>
-        protected virtual IEnumerable<string> CreateCodes(ICodeCreater codeCreater, IEnumerable<Param> parameters)
-        {
-            var codes = codeCreater.Create(parameters);
-            return codes;
-        }
-
-        /// <summary>
-        /// Write codes into file or directory specified by argument outputPath.
-        /// </summary>
-        /// <param name="outputPath">Path to directory or file to output codes.</param>
-        /// <param name="codes"></param>
-        protected virtual void WriteSourceCodes(string outputPath, IEnumerable<string> codes)
-        {
-            var codeWrtier = new CLangSourceStubCodeWriter(outputPath);
-            this.WriteCodes(codeWrtier, codes);
-        }
-
-        protected virtual void WriteHeaderCodes(string outputPath, IEnumerable<string> codes)
-        {
-            var codeWrtier = new CLangHeaderStubCodeWriter(outputPath);
-            this.WriteCodes(codeWrtier, codes);
-        }
-
-        /// <summary>
-        /// Write codes into file or directory specified by argument outputPath.
-        /// </summary>
-        /// <param name="codeWriter">ICodeWriter concrete object to write codes into files.</param>
-        /// <param name="codes">List of codes to write into a file.</param>
-        protected virtual void WriteCodes(ICodeWriter codeWriter, IEnumerable<string> codes)
-        {
-            codeWriter.WriteCode(codes);
+            foreach (var creater in createres)
+            {
+                creater.Create(outputPath, parameters);
+            }
         }
         #endregion
     }
---
+++ dev/dev/CStubMKGui/Model/StubHeaderFileCreater.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CStubMKGui.Model
+{
+	public class StubHeaderFileCreater : ASourceFileCreater
+	{
+		#region Override abstract methods.
+		/// <summary>
+		/// Returns object to create code.
+		/// </summary>
+		/// <returns>Object to create code.</returns>
+		public override ICodeCreater GetCodeCreater()
+		{
+			var creater = new CLangHeaderStubCodeCreater();
+			return creater;
+		}
+
+		/// <summary>
+		/// Retunrs object to write code into a file.
+		/// </summary>
+		/// <param name="outputPath">Path to folder to output a file.</param>
+		/// <returns>Object to write code.</returns>
+		public override ICodeWriter GetCodeWriter(string outputPath)
+		{
+			var writer = new CLangHeaderStubCodeWriter(outputPath);
+			return writer;
+		}
+		#endregion
+	}
+}
