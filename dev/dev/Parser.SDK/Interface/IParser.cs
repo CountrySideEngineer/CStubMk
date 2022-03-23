9ee2448e72d98fe16683843224ace45e39aa1e08
CountrySideEngineer <deep-sleeper@nifty.com>
2022-03-23 22:57:08 +0900

    Start developing Parser.SDK and a DLL using it.

---
+++ dev/dev/Parser.SDK/Interface/IParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reader.SDK.Model;
+using Parser.SDK.Model;
+
+namespace Parser.SDK.Interface
+{
+	public interface IParser
+	{
+		/// <summary>
+		/// Interface to method to parser the function definition into Parameter object.
+		/// </summary>
+		/// <param name="paramInfos">Collection of ParameterInformation contains source of data.</param>
+		/// <returns>Collection of parameter parsed by method.</returns>
+		IEnumerable<Parameter> Parse(IEnumerable<ParameterInformation> paramInfos);
+
+		/// <summary>
+		/// Interface to method to parser the function definition into Parameter object.
+		/// </summary>
+		/// <param name="paramInfo">ParameterInformation contains of data.</param>
+		/// <returns>Parameter object parsed by method.</returns>
+		Parameter Parse(ParameterInformation paramInfo);
+	}
+}
---
+++ dev/dev/Parser.SDK/Model/Parameter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.SDK.Model
+{
+	public class Parameter
+	{
+		/// <summary>
+		/// Data type name.
+		/// </summary>
+		public string DataType { get; set; }
+
+		/// <summary>
+		/// Name of parameter.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// File name the parameter is implemented.
+		/// </summary>
+		public string FileName { get; set; }
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public Parameter()
+		{
+			DataType = string.Empty;
+			Name = string.Empty;
+			FileName = string.Empty;
+		}
+
+		/// <summary>
+		/// Create parameter in string.
+		/// </summary>
+		/// <returns>Parameter in string.</returns>
+		public override string ToString()
+		{
+			string toString = $"{DataType} {Name}";
+			return toString;
+		}
+	}
+}
