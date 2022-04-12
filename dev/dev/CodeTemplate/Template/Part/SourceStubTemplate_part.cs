38ef0f5e5091e42e2a45940059c81a56df249411
CountrySideEngineer <deep-sleeper@nifty.com>
2022-04-12 22:30:42 +0900

    Update CodeTemplate project. -Update SourceStubTemplate class by implementing methods to generate codes of stub methods  using template classes.

--- dev/dev/CodeTemplate/Template/Part/SourceStubTemplate_part.cs
+++ dev/dev/CodeTemplate/Template/Part/SourceStubTemplate_part.cs
@@ -25,7 +25,16 @@ namespace CodeTemplate.Template
 		/// <returns>Code to declare buffers.</returns>
 		public string GenerateBufferDeclareCode()
 		{
-			return string.Empty;
+			try
+			{
+				var template = new StubBufferDeclarationTemplate();
+				string code = GenerateCode(template);
+				return code;
+			}
+			catch (NullReferenceException)
+			{
+				throw;
+			}
 		}
 
 		/// <summary>
@@ -34,16 +43,56 @@ namespace CodeTemplate.Template
 		/// <returns>Codes of method to initialize buffers.</returns>
 		public string GenerateBufferInitCode()
 		{
-			return string.Empty;
+			try
+			{
+				var template = new StubBufferInitTemplate();
+				string code = GenerateCode(template);
+				return code;
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
 		}
 
 		/// <summary>
 		/// Generate stub body.
 		/// </summary>
 		/// <returns>Codes of stub method body.</returns>
+		/// <exception cref="NullReferenceException"></exception>
 		public string GenerateStubCode()
 		{
-			return string.Empty;
+			try
+			{
+				var template = new StubSourceBodyTemplate();
+				string code = GenerateCode(template);
+				return code;
+			}
+			catch (NullReferenceException)
+			{
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Generate code using template.
+		/// </summary>
+		/// <param name="template">Code template file.</param>
+		/// <returns>Generated code.</returns>
+		/// <exception cref="NullReferenceException"></exception>
+		protected virtual string GenerateCode(StubCodeTemplateCommonBase template)
+		{
+			try
+			{
+				template.TargetFunc = TargetFunc;
+				string code = template.TransformText();
+				return code;
+			}
+			catch (NullReferenceException)
+			{
+				throw;
+			}
 		}
 	}
 }
