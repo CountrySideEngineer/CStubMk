c4e9a4e0336a65500d2fac17559cea66df6261a4
CountrySideEngineer <deep-sleeper@nifty.com>
2022-04-17 21:31:05 +0900

    Add class to read function data from text file. -Add FunctionInTextReader class, to read function data from text file. -Add unit test of the class. -Update dll file.

---
+++ dev/dev/Reader/FunctionReader/FunctionInTextReader.cs
@@ -0,0 +1,43 @@
+﻿using Reader.SDK.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader.FunctionReader
+{
+	public class FunctionInTextReader : AFunctionReader
+	{
+		public override IEnumerable<ParameterInformation> Read(string path)
+		{
+			string allCode = GetAllCode(path);
+			IEnumerable<string> codes = ToCodes(allCode);
+
+			throw new NotImplementedException();
+		}
+
+		protected virtual string GetAllCode(string path)
+		{
+			string allCode = string.Empty;
+			using (var reader = new StreamReader(path, System.Text.Encoding.GetEncoding("shift_jis")))
+			{
+				allCode = reader.ReadToEnd();
+			}
+			return allCode;
+		}
+
+		protected virtual IEnumerable<string> ToCodes(string srcCode)
+		{
+			//改行を削除
+			string replacedCode = srcCode.Replace("\r\n", "\n").Replace("\n", "");
+			char[] deliminators = new char[]
+			{
+				';'
+			};
+			IEnumerable<string> codes = replacedCode.Split(deliminators);
+			return codes;
+		}
+	}
+}
---
+++ dev/test/utest/Reader_utest/Reader_utest/FunctionInTextReader_UnitTest_ToCodes_test.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reader.FunctionReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader_utest
+{
+	//[TestClass]
+	public partial class FunctionInTextReader_UnitTest
+	{
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_001()
+		{
+			string srcCode = "int function(void)";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_002()
+		{
+			string srcCode = "int function(void);" +
+				Environment.NewLine +
+				"int function1(int data1);" +
+				Environment.NewLine +
+				Environment.NewLine +
+				"int function2(int data2);";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_003()
+		{
+			string srcCode = "int function(void);" +
+				"int function1(int data1);" +
+				"int function2(int data2);";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_004()
+		{
+			string srcCode = "int function(void);" +
+				"int function1(" +
+				Environment.NewLine +
+				"int data1" +
+				Environment.NewLine +
+				");" +
+				Environment.NewLine +
+				"int function2(" +
+				Environment.NewLine +
+				"int data2" +
+				Environment.NewLine +
+				");";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+	}
+}
