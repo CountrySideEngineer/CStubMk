40beeb39c2ca28bbc7ffdeb3423c0ea5ef244b94
CountrySideEngineer <deep-sleeper@nifty.com>
2022-04-19 04:50:25 +0900

    Update FunctionInTextReader class. -Update the class by converting a code into a method. -Add unit test of the method adding by the update.

--- dev/dev/Reader/FunctionReader/FunctionInTextReader.cs
+++ dev/dev/Reader/FunctionReader/FunctionInTextReader.cs
@@ -14,18 +14,9 @@ namespace Reader.FunctionReader
 		{
 			string allCode = GetAllCode(path);
 			IEnumerable<string> codes = ToCodes(allCode);
+			IEnumerable<ParameterInformation> paramterInformations = ToParameterInformation(codes);
 
-			var paramInformations = new List<ParameterInformation>();
-			foreach (var code in codes)
-			{
-				var paramInformation = new ParameterInformation()
-				{
-					Code = code,
-					File = string.Empty
-				};
-				paramInformations.Add(paramInformation);
-			}
-			return paramInformations;
+			return paramterInformations;
 		}
 
 		protected virtual string GetAllCode(string path)
@@ -41,7 +32,7 @@ namespace Reader.FunctionReader
 		protected virtual IEnumerable<string> ToCodes(string srcCode)
 		{
 			//改行を削除
-			string replacedCode = srcCode.Replace("\r\n", "\n").Replace("\n", "");
+			string replacedCode = srcCode.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "");
 			char[] deliminators = new char[]
 			{
 				';'
@@ -53,8 +44,30 @@ namespace Reader.FunctionReader
 			 * To avoid it, all item in the collection should not be empty.
 			 * So, remove the empty item from the list.
 			 */
-			IEnumerable<string> codes = splittedCode.Where(_ => !string.IsNullOrEmpty(_));
+			IEnumerable<string> codes = 
+				splittedCode.Where(_ => (!string.IsNullOrEmpty(_)) && (!string.IsNullOrWhiteSpace(_)));
 			return codes;
+		}
+
+		protected virtual IEnumerable<ParameterInformation> ToParameterInformation(IEnumerable<string> codes)
+		{
+			var collection = new List<ParameterInformation>();
+			foreach (var code in codes)
+			{
+				ParameterInformation item = ToParameterInformation(code);
+				collection.Add(item);
+			}
+			return collection;
+		}
+
+		protected virtual ParameterInformation ToParameterInformation(string code)
+		{
+			var parameterInfo = new ParameterInformation()
+			{
+				Code = code,
+				File = string.Empty
+			};
+			return parameterInfo;
 		}
 	}
 }
--- dev/test/utest/Reader_utest/Reader_utest/FunctionInTextReader_UnitTest_ToCodes_test.cs
+++ dev/test/utest/Reader_utest/Reader_utest/FunctionInTextReader_UnitTest_ToCodes_test.cs
@@ -21,6 +21,7 @@ namespace Reader_utest
 			var privateReader = new PrivateObject(reader);
 			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
 
+			Assert.AreEqual(1, codes.Count());
 			Assert.AreEqual("int function(void)", codes.ElementAt(0));
 		}
 
@@ -39,6 +40,7 @@ namespace Reader_utest
 			var privateReader = new PrivateObject(reader);
 			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
 
+			Assert.AreEqual(3, codes.Count());
 			Assert.AreEqual("int function(void)", codes.ElementAt(0));
 			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
 			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
@@ -56,6 +58,7 @@ namespace Reader_utest
 			var privateReader = new PrivateObject(reader);
 			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
 
+			Assert.AreEqual(3, codes.Count());
 			Assert.AreEqual("int function(void)", codes.ElementAt(0));
 			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
 			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
@@ -82,6 +85,64 @@ namespace Reader_utest
 			var privateReader = new PrivateObject(reader);
 			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
 
+			Assert.AreEqual(3, codes.Count());
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_005()
+		{
+			string srcCode = "int function(void);" +
+				Environment.NewLine +
+				"int function1(int data1);" +
+				"int function2(int data2);";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual(3, codes.Count());
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_006()
+		{
+			string srcCode = "int function(void);" +
+				"int function1(int data1);" +
+				Environment.NewLine +
+				"int function2(int data2);";
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual(3, codes.Count());
+			Assert.AreEqual("int function(void)", codes.ElementAt(0));
+			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
+			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
+		}
+
+		[TestMethod]
+		[TestCategory("FunctionInTextReader")]
+		[TestCategory("ToCodes")]
+		public void ToCodes_Test_007()
+		{
+			string srcCode = "int function(void);" +
+				"int function1(int data1);" +
+				"int function2(int data2);" +
+				Environment.NewLine;
+			var reader = new FunctionInTextReader();
+			var privateReader = new PrivateObject(reader);
+			IEnumerable<string> codes = (IEnumerable<string>)privateReader.Invoke("ToCodes", srcCode);
+
+			Assert.AreEqual(3, codes.Count());
 			Assert.AreEqual("int function(void)", codes.ElementAt(0));
 			Assert.AreEqual("int function1(int data1)", codes.ElementAt(1));
 			Assert.AreEqual("int function2(int data2)", codes.ElementAt(2));
