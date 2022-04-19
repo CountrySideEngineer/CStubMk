a514e5d612548440958a01037ad9c0f45df17616
CountrySideEngineer <deep-sleeper@nifty.com>
2022-04-19 22:09:16 +0900

    Update binary file.

--- dev/dev/CStubMk/Properties/AssemblyInfo.cs
+++ dev/dev/CStubMk/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
 [assembly: AssemblyTitle("CStubMk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Create stub code in C language .")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CStubMk")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
