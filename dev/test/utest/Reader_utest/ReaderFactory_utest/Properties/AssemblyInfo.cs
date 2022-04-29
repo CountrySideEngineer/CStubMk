f58f317fb06ec320833a80acf5ecfc39c1aaba1b
CountrySideEngineer <deep-sleeper@nifty.com>
2022-04-30 06:34:35 +0900

    Add unit test of ReaderFactory class.

---
+++ dev/test/utest/Reader_utest/ReaderFactory_utest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReaderFactory_utest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReaderFactory_utest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ff879c66-63f7-44f1-955a-1ae84e752a70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
